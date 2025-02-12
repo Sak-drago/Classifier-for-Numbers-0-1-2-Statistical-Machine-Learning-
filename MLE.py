4e9e5a5700df370fe7283f67204a098a008e5696
Sak-drago <sakshatsachdeva@gmail.com>
2025-02-12 15:47:28 +0530

    Task Update: [+] Added Covariance Calculator

--- MLE.py
+++ MLE.py
@@ -6,10 +6,8 @@ flattenImages = data["images"]
 labels = data["labels"]
 
 print(flattenImages.shape)
-print("Max pixel value:", np.max(flattenImages))
 means = np.zeros((3,784), dtype = np.float64)
 
-print(flattenImages)
 
 for idx in range(300):
     if labels[idx] == 0:
@@ -20,4 +18,23 @@ for idx in range(300):
         means[2] += flattenImages[idx]
 
 means = means/100.0
+centralisedMatrix = [[], [], []]
+for idx in range(300):
+    if labels[idx] == 0:
+        centralisedMatrix[0].append(flattenImages[idx] - means[0])
+    elif labels[idx] == 1:
+        centralisedMatrix[1].append(flattenImages[idx] - means[1])
+    elif labels[idx] == 2:
+        centralisedMatrix[2].append(flattenImages[idx] - means[2])
 
+
+covarMatrix = []
+for cl in range(3):
+    centralisedMatrixPerClass = np.array(centralisedMatrix[cl])
+    covarMatrixPerClass = np.dot(centralisedMatrixPerClass.transpose(), centralisedMatrixPerClass) / (100-1)
+    covarMatrix.append(covarMatrixPerClass)
+     
+for row in range(784):
+    for col in range(784):
+        if covarMatrix[0][row][col]>0:
+            print(covarMatrix[0][row][col])
